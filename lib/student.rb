050dc921242d8744dc695090284560dfac1f4358
Diego Vasquez <botellongo69@outlook.com>
2020-02-21 20:05:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
-class Song < InteractiveRecord
+class Student < InteractiveRecord
 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
